76ac444947c771f7280ac7f940b367b498911420
Dennis Ting <dennisting@gmail.com>
2015-07-30 04:13:29 -0700

    Initial commit.

---
+++ app/scripts/main.js
@@ -0,0 +1,182 @@
+(function() {
+  'use strict';
+
+  $(document).ready(init);
+
+  function init() {
+    $.get('http://freegeoip.net/json/', function(response) {
+      var coordinates = {
+        lat: +response.latitude,
+        lon: +response.longitude,
+      };
+      $('#city').html(response.city);
+
+      // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+      getWeather(coordinates, getUnits(response.country_code));
+      $('#region').html(response.region_name);
+
+      // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+    });
+  }
+
+  $('.loadable').hide();
+
+  $('#tempContainer').click(function toggleFC() {
+    var tempDiv = document.getElementById('temp');
+    var tu = tempDiv.innerHTML.split('°');
+    if (tu[1] === 'F') {
+      tempDiv.innerHTML = ((+tu[0] - 32) / 1.8).toFixed(2) + '&deg;' + 'C';
+    } else {
+      tempDiv.innerHTML = ((+tu[0] * 1.8) + 32).toFixed(2) + '&deg;' + 'F';
+    }
+  });
+
+  $('button').click(function refresh() {
+    $('.loadable').fadeOut();
+    init();
+  });
+
+  function getWeather(coords, units) {
+    var weatherAPI = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
+                     coords.lat + '&lon=' + coords.lon + '&units=' + units;
+    $.get(weatherAPI, function(weather) {
+      $('#weatherIcon').html('<img src="http://openweathermap.org/img/w/' +
+                             weather.weather[0].icon + '.png">');
+      $('#temp').html(weather.main.temp + '&deg;' +
+                      (units === 'metric' ? 'C' : 'F'));
+      var description = DESCRIPTIONS[weather.weather[0].icon];
+      $('#description').html(description.text);
+      $('body').css('backgroundImage', 'url("' + description.img + '")');
+      $('#attribution').html(attribution(description));
+      $('#windDirection').html(degToCompass(weather.wind.deg));
+      $('#windSpeed').html(weather.wind.speed + ' knots');
+      $('.loadable').fadeIn();
+    }, 'jsonp');
+  }
+
+  function attribution(description) {
+    return '<a href="' + description.attr + '">' + description.attr + '</a>';
+  }
+
+  function getUnits(country) {
+    console.log(country);
+    var imperialCountries = ['US', 'BS', 'BZ', 'KY', 'PW'];
+    return imperialCountries.indexOf(country) === -1 ? 'metric' : 'imperial';
+  }
+
+  function degToCompass(deg) {
+    return DIR[(~~((deg / 22.5) + 0.5) % 16)];
+  }
+
+  var DIR = [
+    'N',
+    'NNE',
+    'NE',
+    'ENE',
+    'E',
+    'ESE',
+    'SE',
+    'SSE',
+    'S',
+    'SSW',
+    'SW',
+    'WSW',
+    'W',
+    'WNW',
+    'NW',
+    'NNW',
+  ];
+
+  var DESCRIPTIONS = {
+    '01d': {
+      img: 'https://farm8.staticflickr.com/7559/16089993777_8f601d96e2_o.jpg',
+      attr: 'https://www.flickr.com/photos/124329303@N08/',
+      text: 'sky is clear',
+    },
+    '01n': {
+      img: 'https://farm6.staticflickr.com/5604/15481101575_577b9380a3_o.jpg',
+      attr: 'https://www.flickr.com/photos/124329303@N08/',
+      text: 'sky is clear',
+    },
+    '02d': {
+      img: 'https://farm9.staticflickr.com/8391/8476078161_59baaa9355_o.jpg',
+      attr: 'https://www.flickr.com/photos/sdasmarchives/',
+      text: 'few clouds',
+    },
+    '02n': {
+      img: 'https://farm6.staticflickr.com/5465/17214759716_193465c7e1_k.jpg',
+      attr: 'https://www.flickr.com/photos/ryanhallock/',
+      text: 'few clouds',
+    },
+    '03d': {
+      img: 'https://farm4.staticflickr.com/3890/18946819309_64478cf81d_h.jpg',
+      attr: 'https://www.flickr.com/photos/michaelpardo/',
+      text: 'scattered clouds',
+    },
+    '03n': {
+      img: 'https://farm9.staticflickr.com/8711/17336854195_36630c69bf_k.jpg',
+      attr: 'https://www.flickr.com/photos/jdamasio/',
+      text: 'scattered clouds',
+    },
+    '04d': {
+      img: 'https://farm9.staticflickr.com/8061/8231162693_a411f15c51_k.jpg',
+      attr: 'https://www.flickr.com/photos/au_unistphotostream/',
+      text: 'broken clouds',
+    },
+    '04n': {
+      img: 'https://farm8.staticflickr.com/7681/16712423663_4a2ea5ce92_k.jpg',
+      attr: 'https://www.flickr.com/photos/tigra-co/',
+      text: 'broken clouds',
+    },
+    '09d': {
+      img: 'https://farm9.staticflickr.com/8868/18430831370_2b7b070858_h.jpg',
+      attr: 'https://www.flickr.com/photos/michaelpardo/',
+      text: 'shower rain',
+    },
+    '09n': {
+      img: 'https://farm8.staticflickr.com/7696/16895976370_6ae0428987_h.jpg',
+      attr: 'https://www.flickr.com/photos/michaelpardo/',
+      text: 'shower rain',
+    },
+    '10d': {
+      img: 'https://farm2.staticflickr.com/1150/873273143_55c5fcac2f_o.jpg',
+      attr: 'https://www.flickr.com/photos/henriquev/',
+      text: 'rain',
+    },
+    '10n': {
+      img: 'https://farm4.staticflickr.com/3884/14436522280_0096cfd4e2_k.jpg',
+      attr: 'https://www.flickr.com/photos/minoru_ntt/',
+      text: 'rain',
+    },
+    '11d': {
+      img: 'https://farm9.staticflickr.com/8899/18808234182_561f5466c6_k.jpg',
+      attr: 'https://www.flickr.com/photos/nealherbert/',
+      text: 'Thunderstorm',
+    },
+    '11n': {
+      img: 'https://farm8.staticflickr.com/7172/13881379074_adc3f3691a_k.jpg',
+      attr: 'https://www.flickr.com/photos/matthew_eaton/',
+      text: 'Thunderstorm',
+    },
+    '13d': {
+      img: 'https://farm4.staticflickr.com/3500/3251874290_6da9504bef_o.jpg',
+      attr: 'https://www.flickr.com/photos/boklm/',
+      text: 'Snow',
+    },
+    '13n': {
+      img: 'https://farm6.staticflickr.com/5125/5257701655_bbb4790581_o.jpg',
+      attr: 'https://www.flickr.com/photos/pasukaru76/',
+      text: 'Snow',
+    },
+    '50d': {
+      img: 'https://farm8.staticflickr.com/7545/15884841046_442d1f2a2f_k.jpg',
+      attr: 'https://www.flickr.com/photos/mlinksva/',
+      text: 'mist',
+    },
+    '50n': {
+      img: 'https://farm5.staticflickr.com/4026/4479273087_30858afd17_o.jpg',
+      attr: 'https://www.flickr.com/photos/22723959@N08/',
+      text: 'mist',
+    },
+  };
+})();
